9c7866044bb6043f66d2621e29437a056cc3aaf3
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-01-28 09:51:05 +0200

    Modified the startup script

--- startup.sh
+++ startup.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-if pgrep -x "emacs" > /dev/null; then
-    :
+if pgrep -x "dunst" > /dev/null; then
+  :
 else
+  dunst -conf ~/.config/dunst/dunstrc &
+fi
+
+if pgrep -x "emacs" > /dev/null; then
+    notify-send "Emacs Daemon is Running"
+else
+    notify-send "Starting Emacs Daemon"
     emacs --daemon
+    notify-send "Emacs Daemon is Started"
 fi
 
 if pgrep -x "picom" > /dev/null; then
-    :
+    notify-send "Picom is Running"
 else
     picom --config ~/.config/picom/picom.conf &
+    notify-send "Picom is Started"
 fi
 
 feh --bg-scale ~/suckless/Wallpaper.jpg &
-
-# dunst &
 
 slstatus &
 
