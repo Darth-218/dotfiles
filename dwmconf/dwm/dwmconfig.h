5ebafc76b77400a9a3d92b22dbe5173216436dc3
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-02-04 04:52:41 +0200

    Fixed conflicting patches and removed non used

--- dwmconf/dwm/dwmconfig.h
+++ dwmconf/dwm/dwmconfig.h
@@ -2,8 +2,13 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 8;        /* gaps between windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+// static       int smartgaps          = 8;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 10;       /* vertical padding of bar */
@@ -46,11 +51,24 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 static const int attachbelow = 1;    /* 1 means attach after the currently active window */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+// #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+// #include "vanitygaps.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ " ",      tile },    /* first entry is default */
-	{ " ",      NULL },    /* no layout function means floating behavior */
-	{ " ",      monocle },
+	{ " ",      NULL },    /* no layout function means floating behavior */ { " ",      monocle }, /* { "[@]",      spiral },
+	{ "[\\]",     dwindle },
+	{ "H[]",      deck },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+	{ NULL,       NULL }, */
 };
 
 /* key definitions */
@@ -90,10 +108,12 @@ static const char *rofiemojicmd[] = {"rofi", "emoji", "-show", "emoji", NULL};
 static const char *rofiwificmd[] = {"rofi-wifi", NULL};
 static const char *rofipowercmd[] = {"rofi-powermenu", NULL};
 
-
 /* Volume control */
 static const char *volup[]   = { "amixer", "set", "Master", "5%+", NULL };
 static const char *voldown[] = { "amixer", "set", "Master", "5%-", NULL };
+
+/* Lock the screen*/
+static const char *lockscrn[] = {"betterlockscreen", "-l", "blur", NULL};
 
 static Keychord *keychords[] = {
        /* Keys        function        argument */
@@ -167,38 +187,42 @@ static Keychord *keychords[] = {
 
        // super + a
        /// e emacs
-       &((Keychord){2, {{MODKEY, XK_a}, {MODKEY, XK_e}},                       spawn,          {.v = emacsclientcmd } }),
+       &((Keychord){2, {{MODKEY, XK_a}, {0, XK_e}},                       spawn,          {.v = emacsclientcmd } }),
 
        /// b Mercury browser
-       &((Keychord){2, {{MODKEY, XK_a}, {MODKEY, XK_b}},                       spawn,          {.v = mercurycmd } }),
+       &((Keychord){2, {{MODKEY, XK_a}, {0, XK_b}},                       spawn,          {.v = mercurycmd } }),
 
        /// d discord
-       &((Keychord){2, {{MODKEY, XK_a}, {MODKEY, XK_d}},                       spawn,          {.v = discordcmd } }),
+       &((Keychord){2, {{MODKEY, XK_a}, {0, XK_d}},                       spawn,          {.v = discordcmd } }),
 
        /// f thunar
-       &((Keychord){2, {{MODKEY, XK_a}, {MODKEY, XK_f}},                       spawn,          {.v = thunarcmd } }),
+       &((Keychord){2, {{MODKEY, XK_a}, {0, XK_f}},                       spawn,          {.v = thunarcmd } }),
 
        // super + r
        /// d rofi drun menu
-       &((Keychord){2, {{MODKEY, XK_r}, {MODKEY, XK_d}},                                       spawn,          {.v = rofidruncmd } }),
+       &((Keychord){2, {{MODKEY, XK_r}, {0, XK_d}},                                       spawn,          {.v = rofidruncmd } }),
 
        /// r rofi run menu
-       &((Keychord){2, {{MODKEY, XK_r}, {MODKEY, XK_r}},                                       spawn,          {.v = rofiruncmd } }),
+       &((Keychord){2, {{MODKEY, XK_r}, {0, XK_r}},                                       spawn,          {.v = rofiruncmd } }),
 
        /// c rofi run menu
-       &((Keychord){2, {{MODKEY, XK_r}, {MODKEY, XK_c}},                                       spawn,          {.v = roficalccmd } }),
+       &((Keychord){2, {{MODKEY, XK_r}, {0, XK_c}},                                       spawn,          {.v = roficalccmd } }),
 
        /// f rofi run menu
-       &((Keychord){2, {{MODKEY, XK_r}, {MODKEY, XK_f}},                                       spawn,          {.v = rofifilescmd } }),
+       &((Keychord){2, {{MODKEY, XK_r}, {0, XK_f}},                                       spawn,          {.v = rofifilescmd } }),
 
        /// e rofi run menu
-       &((Keychord){2, {{MODKEY, XK_r}, {MODKEY, XK_e}},                                       spawn,          {.v = rofiemojicmd } }),
+       &((Keychord){2, {{MODKEY, XK_r}, {0, XK_e}},                                       spawn,          {.v = rofiemojicmd } }),
 
        /// w rofi run menu
-       &((Keychord){2, {{MODKEY, XK_r}, {MODKEY, XK_w}},                                       spawn,          {.v = rofiwificmd } }),
+       &((Keychord){2, {{MODKEY, XK_r}, {0, XK_w}},                                       spawn,          {.v = rofiwificmd } }),
 
        /// p rofi run menu
-       &((Keychord){2, {{MODKEY, XK_r}, {MODKEY, XK_p}},                                       spawn,          {.v = rofipowercmd } }),
+       &((Keychord){2, {{MODKEY, XK_r}, {0, XK_p}},                                       spawn,          {.v = rofipowercmd } }),
+
+
+       /* Lockscreen */
+       &((Keychord){1, {{MODKEY, XK_s}},                                                  spawn,          {.v = lockscrn } }),
 };
 
 
