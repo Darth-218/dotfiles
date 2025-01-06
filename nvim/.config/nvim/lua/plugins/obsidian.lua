52b7b0041209ddfd03575194ce3512fe1a883a6b
darth-218 <yahia.hany.gaber@proton.me>
2025-01-06 21:01:37 +0200

    Modified obsidian.nvim config

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -7,17 +7,22 @@ return {
     "nvim-lua/plenary.nvim",
   },
   opts = {
+    -- disable_frontmatter = true,
+    note_frontmatter_func = function(note)
+      return note.tags
+    end,
+
     templates = {
       folder = "~/.config/nvim/lua/plugins/obsidian-temps",
     },
     workspaces = {
       {
-        name = "personal",
-        path = "~/the-duat/Notes",
+	name = "personal",
+	path = "~/the-duat/Notes",
       },
     },
   },
-    init = function()
-    	vim.opt.conceallevel = 2
-    end,
+  init = function()
+    vim.opt.conceallevel = 2
+  end,
 }
