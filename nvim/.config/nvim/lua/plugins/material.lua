92c816973261372e58e0f5b0a89ccf26a6336926
darth-218 <yahia.hany.gaber@proton.me>
2025-01-08 11:23:23 +0200

    Updated material.nvim

--- nvim/.config/nvim/lua/plugins/material.lua
+++ nvim/.config/nvim/lua/plugins/material.lua
@@ -1,3 +1,9 @@
 return {
   "marko-cerovac/material.nvim",
+
+  -- require("material").setup({
+  --   custom_colors = function(colors)
+  --     colors.editor.bg = "#1e1d2f"
+  --   end,
+  -- })
 }
