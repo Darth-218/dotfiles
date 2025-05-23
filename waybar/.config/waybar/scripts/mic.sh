f168c90f37a3a92f81389c18c5b6d3ddb4c71bbb
darth-218 <yahia.hany.gaber@proton.me>
2025-05-23 17:39:11 +0300

    Minor UI tweaks and added a mic module

---
+++ waybar/.config/waybar/scripts/mic.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get default source mute status
+status=$(pactl get-source-mute @DEFAULT_SOURCE@ | awk '{print $2}')
+
+if [ "$status" = "yes" ]; then
+    echo "-"
+else
+    echo "+"
+fi
